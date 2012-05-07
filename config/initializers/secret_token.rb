6d03183bd005688f3cadc4a27c460c7f0913b613
Mike Bloy <mike@bloy.org>
2012-05-07 15:20:44 -0500

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sandwichshop::Application.config.secret_token = '9166dbc5c02546fc1365156ab1940fa2e5a2aa309d17dc15987fe03e0e86dd0a0d457d839eb32726dbcfd6887214e84b1364a83674b206abaca9b4807409f5f7'
