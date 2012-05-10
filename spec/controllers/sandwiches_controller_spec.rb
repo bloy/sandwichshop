a8302a53b2a014af39845451cca3cda07c299d8b
Mike Bloy <mike@bloy.org>
2012-05-10 12:38:46 -0500

    partial spec for sandwich controller

--- spec/controllers/sandwiches_controller_spec.rb
+++ spec/controllers/sandwiches_controller_spec.rb
@@ -1,23 +1,36 @@
 require 'spec_helper'
 
 describe SandwichesController do
-
+  let(:sandwich) { FactoryGirl.create(:sandwich) }
   describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
+    describe "not logged in", :filter => :require_user do
+      before(:each) { get 'show', :id => sandwich.id }
+    end
+
+    describe "logged in", :user => :normal do
+      before(:each) { get 'show', :id => sandwich.id }
+      it { should respond_with(:success) }
+      it { should render_template(:show) }
+      it { should assign_to(:sandwich).with(sandwich) }
     end
   end
 
   describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
+    describe "not logged in", :filter => :require_user do
+      before(:each) { get 'new' }
+    end
+
+    describe "logged in", :user => :normal do
+      before(:each) { get 'new' }
+      it { should respond_with(:success) }
+      it { should render_template(:show) }
+      it { should assign_to(:sandwich).with_kind_of(Sandwich) }
     end
   end
 
   describe "POST 'create'" do
     it "returns http success" do
+      pending
       post 'create'
       response.should be_success
     end
@@ -25,6 +38,7 @@ describe SandwichesController do
 
   describe "GET 'index'" do
     it "returns http success" do
+      pending
       get 'index'
       response.should be_success
     end
