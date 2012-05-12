e678de37fba4f1bbde6168065b5b0cca2d8a36cb
Mike Bloy <mike@bloy.org>
2012-05-11 22:17:08 -0500

    better welcome text

--- spec/views/user_sessions/index.html.erb_spec.rb
+++ spec/views/user_sessions/index.html.erb_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 
 describe "user_sessions/index.html.erb" do
-  it "contains text that tells the user to log in" do
-    render
-    rendered.should =~ /registered and logged in/
+  describe "logged out", :user => :anonymous do
+    it "contains text that tells the user to log in" do
+      view.stub(:logged_in? => false)
+      render
+      rendered.should =~ /registered and logged in/
+    end
+  end
+
+  describe "logged in", :user => :normal do
+    it "contains text telling you place an order" do
+      view.stub(:logged_in? => true)
+      render
+      rendered.should =~ /using the links/
+    end
   end
 end
